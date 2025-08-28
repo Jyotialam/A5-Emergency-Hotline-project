f22ccfb695b13a6482405791dbd0a7c310f3df4d
Zulekha Akter <jyotialam122@gmail.com>
2025-08-28 22:03:47 +0600

    added copy buttons functionality

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let count = 0;
+// heart functionality
+let heartCount = 0;
 const heartBtns = document.getElementsByClassName("heart-btn");
 const heartCounter = document.getElementById("heart-counter");
 for (const heartBtn of heartBtns) {
   heartBtn.addEventListener("click", function () {
-    count++;
-    heartCounter.innerText = count;
+    heartCount++;
+    heartCounter.innerText = heartCount;
   });
 }
-// copy btn+copy counter
+// copy button functionality
+let copyCount = 0;
+const copyBtns = document.getElementsByClassName("copy-btn");
+const copyCounter = document.getElementById("copy-counter");
+for (const copyBtn of copyBtns) {
+  copyBtn.addEventListener("click", function () {
+    copyCount++;
+    copyCounter.innerText = copyCount;
+    //
+    alert("Copied number");
+  });
+}
